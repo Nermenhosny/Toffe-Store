b4134f8183eef803885ff9118ad71c06a4d6bda2
Nermenhosny <102307459+Nermenhosny@users.noreply.github.com>
2023-05-09 21:15:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.Hashtable;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Create a new orderManager object
+        UserManagementMenu menu = new UserManagementMenu();
+         menu.displayMenu();
+    }
+}
+
